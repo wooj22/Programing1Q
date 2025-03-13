f76d0085328912c3fa4ff3036b74047b53713175
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-03-13 15:08:17 +0900

    파일 읽기모드, 쓰기모드

--- Project_0307_1/Project_0307_1/FileIOTest.cpp
+++ Project_0307_1/Project_0307_1/FileIOTest.cpp
@@ -8,14 +8,52 @@ using namespace std;
 // ex) 명령 프롬프트에서 Cmd에서 실행파일의 경로까지 진입하지 않고 경로까지 포함하여 exe를 실행하는경우
 // ex) 바로가기를 만들고 작업 디렉토리를 따로 설정하는 경우
 // 리소스 파일을 참조할때 프로젝트 바깥에 리소스 폴더를 따로 만들어서 해당 디렉토리를 참조하도록 함
-
-int main() {
+void DirectoryCheak() {
 	char szBuffer1[MAX_PATH] = { 0, };
 	char szBuffer2[MAX_PATH] = { 0, };
 	GetModuleFileNameA(NULL, szBuffer1, MAX_PATH);
 	GetCurrentDirectoryA(MAX_PATH, szBuffer2);
 
 	cout << "GetModuleFileNameA : " << szBuffer1 << '\n' << "GetCurrentDirectoryA : " << szBuffer2;
+}
 
+
+// 파일 입출력 쓰기모드 (파일없으니까 실행 ㄴㄴ)
+int FileControll_W() {
+	FILE* fp = fopen("example.txt", "w");
+	if (fp == NULL) return;
+
+	fprintf(fp, "이름 : 홍길동, 점수 : %d\n", 95);
+	fputs("hello!", fp);
+	fputs("A", fp);
+
+	fclose(fp);
+}
+
+// 파일 입출력 읽기모드 (파일없으니까 실행 ㄴㄴ)
+int FileControll_W() {
+	FILE* fp = fopen("example.txt", "r");
+	if (fp == NULL) return;
+
+	char name[50];
+	int score;
+
+	fscanf(fp, "이름: %s, 점수: %d\n", name, &score);	// 정확하게 일치해야함
+	printf("읽은 값 -름: %s, 점수: %d\n", name, &score);
+
+	char buffer[100];
+	fgets(buffer, sizeof(buffer), fp);
+	printf("읽은 줄: %s", buffer);
+
+	char ch = fgetc(fp);
+	printf("읽은 문자 : %c\n", ch);
+
+	fclose(fp);
+}
+
+/*
+int main() {
+	//DirectoryCheak();
 	return 0;
 }
+*/
